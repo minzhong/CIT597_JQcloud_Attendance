7ddfeecc1963c34c862fe2d1c2160e7ff3fd0a86
Min Zhong <minzhong374143@gmail.com>
2015-04-06 06:17:25 -0400

    add more scenarios to AddStudent.feature

--- features/AddStudent.feature
+++ features/AddStudent.feature
@@ -3,4 +3,14 @@ Feature: add a student page
 	Scenario: Add a student
            Given I am on 'create new student page'
             When I add a new student
-            Then I should see a new student page
+            Then I should see Student was successfully created.
+
+	Scenario: Add a student without a first_name
+           Given I am on 'create new student page'
+            When I add a new student with blank first name
+            Then I should see Student was successfully created.with blank first name. 
+
+	Scenario: Add a student without an email
+           Given I am on 'create new student page'
+            When I add a new student with blank email
+            Then I should see Student was successfully created. with blank email. 
--- features/step_definitions/student_steps.rb
+++ features/step_definitions/student_steps.rb
@@ -11,8 +11,33 @@ When(/^I add a new student$/) do
   click_button 'Create Student' 
 end
 
-Then(/^I should see a new student page$/) do
+Then(/^I should see Student was successfully created\.$/) do
  assert page.has_content?("Student was successfully created")
 end
 
 
+When(/^I add a new student with blank first name$/) do
+  fill_in 'First name', :with => ""
+  fill_in 'Last name', :with => "Elliot"
+  fill_in 'Pennid', :with => "12345"
+  fill_in 'Email', :with => "x@y.edu"
+  fill_in 'Note', :with => "Perfect Attendance"
+  click_button 'Create Student' 
+end
+
+Then(/^I should see Student was successfully created\.with blank first name\.$/) do
+ assert page.has_content?("Student was successfully created")
+end
+
+When(/^I add a new student with blank email$/) do
+  fill_in 'First name', :with => "C.S."
+  fill_in 'Last name', :with => "Louis"
+  fill_in 'Pennid', :with => "123457"
+  fill_in 'Email', :with => ""
+  fill_in 'Note', :with => "Perfect Attendance"
+  click_button 'Create Student' 
+end
+
+Then(/^I should see Student was successfully created\. with blank email\.$/) do
+ assert page.has_content?("Student was successfully created")
+end
