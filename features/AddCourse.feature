0eab4de0d0b6fb0cfa2d60a129e920af08dd9068
sosaxena <sosaxena@seas.upenn.edu>
2015-04-05 21:44:50 -0400

    adding faeture file

--- features/AddCourse.feature
+++ features/AddCourse.feature
@@ -1,6 +1,20 @@
 Feature: add a course page
-
+    
 	Scenario: Add a course 
            Given I am on 'create new course page'
             When I add a new course 
             Then I should see a new course page
+
+    Scenario: Add another course 
+           Given I am on the 'create new course page'
+           When I add another new course 
+           Then I should see another new course page
+
+    Scenario: Add a blank name for a course 
+           Given I am adding a course with a blank name
+           When I add a the course
+           Then it should still see a new course page
+    
+   
+
+    
--- features/step_definitions/course_steps.rb
+++ features/step_definitions/course_steps.rb
@@ -12,5 +12,44 @@ When(/^I add a new course$/) do
 end
 
 Then(/^I should see a new course page$/) do
- assert page.has_content?("Course was successfully created.")
+  assert page.has_content?("Course was successfully created.")
+end
+
+
+
+Given(/^I am on the 'create new course page'$/) do
+   visit(new_course_path)
+end
+
+When(/^I add another new course$/) do
+  fill_in 'Course code', :with => "CIS555"
+  fill_in 'Course name', :with => "IWS"
+  fill_in 'Term', :with => "Spring"
+  fill_in 'Year', :with => "2015"
+  fill_in 'Note', :with => "CIS course"
+  click_button 'Create Course'
+end
+
+Then(/^I should see another new course page$/) do
+   assert page.has_content?("Course was successfully created.")
+end
+
+
+
+
+Given(/^I am adding a course with a blank name$/) do
+  visit(new_course_path)
+end
+
+When(/^I add a the course$/) do
+  fill_in 'Course code', :with => "CIS573"
+  fill_in 'Course name', :with => "     "
+  fill_in 'Term', :with => "Fall"
+  fill_in 'Year', :with => "2015"
+  fill_in 'Note', :with => "CIS course"
+  click_button 'Create Course'
+end
+
+Then(/^it should still see a new course page$/) do
+  assert page.has_content?("Course was successfully created.")
 end
