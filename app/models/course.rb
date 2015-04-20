a11800d04665aa03a718653f7a04d14b706ba41f
ashlyenglish <dengxiasummer@sina.com>
2015-04-20 02:22:16 -0400

    Update course.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -14,16 +14,16 @@
 
 class Course < ActiveRecord::Base
  belongs_to :user
- has_many :attendances
- has_and_belongs_to_many :students
+   has_many :attendances, foreign_key: "course_id"
+   has_and_belongs_to_many :students, join_table: "courses_students"
+   # Validations
+   validates :course_code, :presence => true
+   validates :course_name, :presence => true
 
- validates :course_code, :presence => true
- validates :course_name, :presence => true
+   validates :term, :presence => true
+   validates :term, format: { with: /Summer|Fall|Spring/i, message: "term can be one of Summer, Fall or Spring"}
 
- validates :term, :presence => true
- validates :term, format: { with: /Summer|Fall|Spring/i,message: "term can be one of Summer, Fall or Spring"}
- 
- validates :year, :presence => true
- validates :year, format: { with: /\d{4}/,message: "year should have 4 digits" }
+   validates :year, :presence => true
+   validates :year, format: { with: /(1|2)\d{3}/, message: "year format is invalid" }
 end
 
