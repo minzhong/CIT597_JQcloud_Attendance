4f3204635d52394f63e2e67e85cf2f65aac1b74a
ashlyenglish <dengxiasummer@sina.com>
2015-04-20 02:25:08 -0400

    Update attendance.rb

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -15,15 +15,15 @@
 class Attendance < ActiveRecord::Base
   belongs_to :course
   belongs_to :student
-
+                                    
   validates :att_date, :presence => true
-  validates :att_date, format: {with: /(\d{2}|\d{1})\/(\d{2}|\d{1})\/(\d{4})/}
+  #validates :att_date, format: {with: /\d{2}\/\d{2}\/\d{4}/,message:"Date should be mm/dd/yyyy"}
 
   validates :att_time, :presence => true
-  validates :att_time, format: {with: /[0-2][0-3]:[0-5][0-9]/}
-  
+  validates :att_time, format: {with: /[0-2][0-9]:[0-5][0-9]/}
+
   validates :course_id, :presence => true
 
   validates :student_id, :presence => true
-  validates :student_id, format: { with: /\d{8}/,message: "penn ID should be 8 digits" }
+  validates :student_id, format: { with: /\d{8}/, message: "penn ID should be 8 digits" }
 end
