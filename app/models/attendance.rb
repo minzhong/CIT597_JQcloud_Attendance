e5a7cdae209e48590704dca2c4ecd812af650938
Min Zhong <minzhong374143@gmail.com>
2015-04-20 11:19:31 -0400

    change attendance.rb

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -16,7 +16,7 @@ class Attendance < ActiveRecord::Base
   belongs_to :course
   belongs_to :student
                                     
-  validates :att_date, :presence => true
+  #validates :att_date, :presence => true
   #validates :att_date, format: {with: /\d{2}\/\d{2}\/\d{4}/,message:"Date should be mm/dd/yyyy"}
 
   validates :att_time, :presence => true
