f732e8b305043e032ca31ff4b0826e4f346d1fa3
ashlyenglish <dengxiasummer@sina.com>
2015-05-06 14:06:57 -0400

    Update attendance.rb

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -28,21 +28,19 @@ class Attendance < ActiveRecord::Base
   # validates :student_id, format: { with: /\d{8}/, message: "penn ID should be 8 digits" }
   
   require 'csv'
-  require 'date'
-  require 'time'
+  
   def self.import(file, course_id)
     if (file != nil) then
       CSV.foreach(file.path, headers: true) do |row|
         attendance_hash = row.to_hash       
-        p attendance_hash
         # attendance_hash:
         # {"Event Tran Actual DateTime"=>"1/16/15 12:21", "Customer Number"=>"11100125", "Customer Name"=>"GREEN", "Profit Center Name"=>"EMILY", "POS Name"=>"Attendance", "Event Name"=>"AnnenbergAttend2", "Event Tran Type"=>"Attendance", "Entry Type"=>"Regular Entry"}
         # attendance_date_time = ["3/24/15", "12:21"] 
         attendance_date_time = attendance_hash["Event Tran Actual DateTime"]
         attendance_date_time_array = attendance_hash["Event Tran Actual DateTime"].split(" ")
         # Note: convert the string date to date type before saving to db, samething with time
-        attendance_date = Date.strptime(attendance_date_time_array[0], '%m/%d/%Y')  
-        attendance_time = Time.strptime(attendance_date_time, '%m/%d/%Y %H:%M')
+        attendance_date = attendance_date_time_array[0]  
+        attendance_time = attendance_date_time_array[1]
         
         # student_id can be found through attendance_pennid using find_by_id
         attendance_pennid = attendance_hash["Customer Number"]
@@ -55,12 +53,21 @@ class Attendance < ActiveRecord::Base
         else @student_id = @student.first.id
         end
         # check if attendance records for this course already in db
-        attendance = Attendance.where("student_id = ? AND course_id = ? AND att_date = ? AND att_time = ?", Student.find_by_pennid(attendance_hash["Customer Number"]).id, course_id, attendance_date, attendance_time)
+        attendance = []
+        student = Student.find_by_pennid(attendance_hash["Customer Number"]) # check if the student is in the Student table
         
-        if attendance.blank? then # meaning the attendance not exists in the Attendances table
-          @attendance = Attendance.create!({att_date: attendance_date, att_time: attendance_time, note: "", course_id: course_id, student_id: @student_id})
-        else # otherwise create the attendance record
-          attendance.first.update_attributes(att_date: attendance_date, att_time: attendance_time, note: "", course_id: course_id, student_id: @student_id) # update the attendance record found
+        if student != nil then # the student exists
+          attendance = Attendance.where("student_id = ? AND course_id = ? AND att_date = ? AND att_time = ?", student.id, course_id, attendance_date, attendance_time)
+        end 
+        
+        if attendance.blank? # not in db yet
+          if student != nil # meaning the student registered for the course and the attendance table does not exist in the Attendance db
+            # if student.courses.include? (Course.find_by_id(course_id))
+               @attendance = Attendance.create!({att_date: attendance_date, att_time: attendance_time, note: "", course_id: course_id, student_id: @student_id})
+            # end
+          end    
+        else # attendance already exist
+            attendance.first.update_attributes(att_date: attendance_date, att_time: attendance_time, note: "", course_id: course_id, student_id: @student_id) # update the attendance record found
         end 
         
       end # end CSV.foreach
@@ -72,4 +79,4 @@ class Attendance < ActiveRecord::Base
     return counts
   end
 
-end
+end # end of class
